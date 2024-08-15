3a482abbe02145b619980f50469347c8da378926
Antonella Cortazzo <anttocortazzo14@gmail.com>
2024-08-15 18:54:36 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,9 @@
+// Validar que la contraseña tenga al menos 6 caracteres
+if (password1.length < 6) {
+        showAlertError();
+        return;
+    }
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
